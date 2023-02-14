e38d6725c0755323dc6fe0bad26ebd9af913f334
Lizzy <elizabethpine4@gmail.com>
2023-02-13 21:20:28 -1000

    Finished writing regex to replicate links, bold/italics, link breaks, and remove unnecessary HTML tags

--- src/components/LessonDetail.jsx
+++ src/components/LessonDetail.jsx
@@ -9,65 +9,106 @@ function LessonDetail() {
   const { data } = useGetLessonDetailQuery(params.lessonId)
   const navigate = useNavigate()
 
-  // const lessonNames = {
-  //   Compromise: 2,
-  //   Steadfastness: 3,
-  //   Integrity: 4,
-  //   Seriousness: 5,
-  //   Focus: 6,
-  //   'Doing Your Best': 7,
-  //   'Pursuing Goals': 8,
-  //   Self: 9,
-  //   Others: 10,
-  //   Work: 11,
-  //   Checkbooks: 12,
-  //   Debt: 13,
-  //   Paychecks: 14,
-  //   Benefits: 15,
-  //   Taxes: 16,
-  //   Investment: 17,
-  //   Accountant: 18,
-  //   'Real Numbers': 19
-  // }
-
   // If one of the lesson names renders, attach a link to it to the appropriate lesson.
   const lessonNames = {
     "<a href='LessonDetails.aspx?GUID=&Less=2'>Compromise</a>": 'Compromise',
+    "<a href='LessonDetails.aspx?GUID=&Less=3'>Steadfastness</a>": 'Steadfastness',
+    "<a href='LessonDetails.aspx?GUID=&Less=4'>Integrity</a>": 'Integrity',
+    "<a href=' LessonDetails.aspx?GUID=&Less=4'>Integrity</a>": 'Integrity',
+    "<a href='LessonDetails.aspx?GUID=&Less=5'>Seriousness</a>": 'Seriousness',
+    "<a href='LessonDetails.aspx?GUID=&Less=6'>Focus</a>": 'Focus',
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Seriousness</a>": 'Seriousness',
+    "<a href='LessonDetails.aspx?GUID=&Less=7'>Doing Your Best</a>": 'Doing Your Best',
+    "<a href='LessonDetails.aspx?GUID=&Less=8'>Pursuing Goals</a>": 'Pursuing Goals',
+    "<a href=' LessonDetails.aspx?GUID=&Less=8'>Goals</a>": 'Pursuing Goals',
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Self</a>": 'Self',
+    "<a href=' LessonDetails.aspx?GUID=&Less=9'>Self</a>": 'Self',
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Integrity</a>": 'Integrity',
+    "<a href=' LessonDetails.aspx?GUID=&Less=10'>Others</a>": 'Others',
+    "<a href='LessonDetails.aspx?GUID=&Less=11'>Work</a>": 'Work',
+    "<a href='LessonDetails.aspx?GUID=&Less=12'>Checkbooks</a>": 'Checkbooks',
     "<a href='LessonDetails.aspx?GUID=&Less=13'> Debt</a>": 'Debt',
+    "<a href='LessonDetails.aspx?GUID=&Less=14'> Paychecks</a>": 'Paychecks',
+    "<a href='LessonDetails.aspx?GUID=&Less=15'> Benefits</a>": 'Benefits',
+    "<a href='LessonDetails.aspx?GUID=&Less=13'> Taxes</a>": 'Taxes',
+    "<a href='LessonDetails.aspx?GUID=&Less=16'>Taxes</a>": 'Taxes',
+    "<a href='LessonDetails.aspx?GUID=&Less=16'> Taxes</a>": 'Taxes',
+    "<a href='LessonDetails.aspx?GUID=&Less=17'>Investment</a>": 'Investment',
     "<a href='LessonDetails.aspx?GUID=&Less=18'>Accountant</a>": 'Accountant',
-    "<a href='LessonDetails.aspx?GUID=&Less=18'> Accountant</a>": 'Accountant'
+    "<a href='LessonDetails.aspx?GUID=&Less=18'> Accountant</a>": 'Accountant',
+    "<a href='LessonDetails.aspx?GUID=&Less=19'>Real Numbers</a>": 'Real Numbers'
   }
 
   const lessonNumbers = {
     "<a href='LessonDetails.aspx?GUID=&Less=2'>Compromise</a>": 2,
+    "<a href='LessonDetails.aspx?GUID=&Less=3'>Steadfastness</a>": 3,
+    "<a href='LessonDetails.aspx?GUID=&Less=4'>Integrity</a>": 4,
+    "<a href=' LessonDetails.aspx?GUID=&Less=4'>Integrity</a>": 4,
+    "<a href='LessonDetails.aspx?GUID=&Less=6'>Focus</a>": 6,
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Focus</a>": 6,
+    "<a href='LessonDetails.aspx?GUID=&Less=7'>Doing Your Best</a>": 7,
+    "<a href='LessonDetails.aspx?GUID=&Less=8'>Pursuing Goals</a>": 8,
+    "<a href=' LessonDetails.aspx?GUID=&Less=8'>Goals</a>": 8,
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Self</a>": 9,
+    "<a href=' LessonDetails.aspx?GUID=&Less=9'>Self</a>": 9,
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Seriousness</a>": 5,
+    "<a href='LessonDetails.aspx?GUID=&Less=9'>Integrity</a>": 4,
+    "<a href=' LessonDetails.aspx?GUID=&Less=10'>Others</a>": 10,
+    "<a href='LessonDetails.aspx?GUID=&Less=11'>Work</a>": 11,
+    "<a href='LessonDetails.aspx?GUID=&Less=12'>Checkbooks</a>": 12,
     "<a href='LessonDetails.aspx?GUID=&Less=13'> Debt</a>": 13,
+    "<a href='LessonDetails.aspx?GUID=&Less=14'> Paychecks</a>": 14,
+    "<a href='LessonDetails.aspx?GUID=&Less=15'> Benefits</a>": 15,
+    "<a href='LessonDetails.aspx?GUID=&Less=13'> Taxes</a>": 16,
+    "<a href='LessonDetails.aspx?GUID=&Less=16'>Taxes</a>": 17,
+    "<a href='LessonDetails.aspx?GUID=&Less=16'> Taxes</a>": 17,
+    "<a href='LessonDetails.aspx?GUID=&Less=17'>Investment</a>": 17,
     "<a href='LessonDetails.aspx?GUID=&Less=18'>Accountant</a>": 18,
-    "<a href='LessonDetails.aspx?GUID=&Less=18'> Accountant</a>": 18
+    "<a href='LessonDetails.aspx?GUID=&Less=18'> Accountant</a>": 18,
+    "<a href='LessonDetails.aspx?GUID=&Less=19'>Real Numbers</a>": 19
   }
 
-  // Fix Accountant with and without Space. There are examples of both
-  // Add bold and italicized text using reactStringReplace
-  // Check against every page on the current website
-
   function formatText(responseText) {
-    const newText = responseText
-      .replaceAll('<b>', ' ')
-      .replaceAll('</b>', ' ')
-      .replaceAll('<i>', ' ')
-      .replaceAll('</i>', ' ')
-      .replaceAll(';', ' ')
+    let replacedText = responseText
 
-    const newText2 = reactStringReplace(
-      newText,
-      /(<a href='LessonDetails.aspx\?GUID=&Less=\d+'>[^<]*<\/a>)/g,
-      (match) => (
-        <a key={lessonNumbers[match]} href={lessonNumbers[match]}>
+    // Activate links
+    replacedText = reactStringReplace(
+      replacedText,
+      /(<a href='\s?LessonDetails.aspx\?GUID=&Less=\d+'>[^<]*<\/a>)/g,
+      (match, i) => (
+        <a key={lessonNumbers[match] + i} className="live-link" href={lessonNumbers[match]}>
           {lessonNames[match]}
         </a>
       )
     )
 
-    return reactStringReplace(newText2, /(<br \/>)/g, (match, i) => <br />)
+    // // Add line breaks where originally intended.
+    replacedText = reactStringReplace(replacedText, /([;])/g, (match, i) => <p key={match + i}></p>)
+    replacedText = reactStringReplace(replacedText, /(<br \/>)/g, (match, i) => (
+      <p key={match + i}></p>
+    ))
+
+    // Italicize text that originally had <i> tags
+    replacedText = reactStringReplace(replacedText, /(?<=<i>)(.*?)(?=<\/i>)/g, (match, i) => (
+      <i key={match + i}>{match}</i>
+    ))
+
+    // Bold text that originally had <b> tags
+    replacedText = reactStringReplace(replacedText, /(?<=<b>)(.*?)(?=<\/b>)/g, (match, i) => (
+      <b key={match + i}>{match}</b>
+    ))
+
+    // Remove the remaining <i>, <b> and <br /> tags
+    replacedText = reactStringReplace(replacedText, /(<i>|<\/i>|<b>|<\/b>|<\/b>)/g, (match, i) => (
+      <span key={match + i}></span>
+    ))
+
+    // Bold the tip on the checkbooks detail page.
+    replacedText = reactStringReplace(replacedText, /(Tip[^]*.)/g, (match, i) => (
+      <b key={match + i}>{match}</b>
+    ))
+
+    return replacedText
   }
   return (
     <div className="container-fluid subjects-wrapper d-flex flex-column col flex-wrap align-items-center">
@@ -90,7 +131,6 @@ function LessonDetail() {
                 className="d-flex flex-column justify-content-center align-items-center"
                 key={detail.LessonID}>
                 <motion.h1
-                  key={detail.LessonID}
                   initial={{ y: 800 }}
                   animate={{ y: 0 }}
                   transition={{ duration: 0.2, ease: 'easeOut' }}
