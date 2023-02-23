6384904a912f3f69f0216ae2b192d5c65676a414
Lizzy <elizabethpine4@gmail.com>
2023-02-22 14:33:39 -1000

    Updated babelrc

--- src/babel.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  presets: [['@babel/plugin-syntax-jsx', { targets: { node: 'current' } }]]
-}
--- src/testFile.test.js
+++ src/testFile.test.js
@@ -1,24 +1,4 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-const fizz_buzz = require('./index')
+const fizz_buzz = require('./index.js')
 
 describe('FizzBuzz', () => {
   test('[3] should result in "fizz"', () => {
